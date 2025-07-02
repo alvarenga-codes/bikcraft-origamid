a158eef8ad3d9c732b25e1b9359f510ee4d940f9
Rodrigo Alvarenga <rodrigo.ribeiroa@gmail.com>
2025-07-02 16:05:52 -0300

    commit script trocarImagem

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 //underline ativo para item do menu ativo
-const links = document.querySelectorAll('.header-menu a');
+const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
-  if(url.includes(href)) {
-    link.classList.add('ativo');
+  if (url.includes(href)) {
+    link.classList.add("ativo");
   }
-};
+}
 
 links.forEach(ativarLink);
 
@@ -16,30 +16,48 @@ links.forEach(ativarLink);
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
-const elemento = document.getElementById(parametro);
-if (elemento) {
-elemento.checked = true;
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
 }
-};
 
 parametros.forEach(ativarProduto);
 
 //Perguntas Frequentes
 
-const perguntas = document.querySelectorAll('.perguntas button');
+const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(event) {
   const pergunta = event.currentTarget;
   const controls = pergunta.getAttribute("aria-controls");
   const resposta = document.getElementById(controls);
 
-  resposta.classList.toggle('ativa');
-  const ativa = resposta.classList.contains("ativa")
-  pergunta.setAttribute('aria-expanded', ativa);
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
 }
 
 function eventosPerguntas(pergunta) {
-  pergunta.addEventListener('click', ativarPergunta);
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//Galeria de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
